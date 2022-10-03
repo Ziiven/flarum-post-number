70da244006b91f4ae53463ce9a40ebb8cc3279db
Ziven <>
2022-10-03 14:17:28 +0800

    v0.1.2

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -4,15 +4,7 @@ import CommentPost from 'flarum/common/components/CommentPost';
 
 app.initializers.add('post-number', () => {
   extend(CommentPost.prototype, 'headerItems', function(items){
-    console.log(items);
-
     const postNumberText = app.translator.trans('post-number.forum.number-prefix', {number: this.attrs.post.data.attributes.number});
-
-    console.log(postNumberText);
-    // items.add(
-    //   'post-number',
-    //   this.attrs.post.data.attributes.number
-    // );
 
     items.add(
       'post-number',
